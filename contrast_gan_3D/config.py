397f75d6e01297625b68b8c6197d1fb5a430081c
xqz-u <marco.gallo0530@gmail.com>
2024-07-04 22:09:28 +0200

    feat: use common paths from configuration file.

--- contrast_gan_3D/config.py
+++ contrast_gan_3D/config.py
@@ -1,11 +1,16 @@
 from pathlib import Path
 
-ROOT_DIR = Path("./").resolve()
+PROJECT_DIR = Path("./").resolve()
 
-LOGS_DIR = ROOT_DIR / "logs"
-
+LOGS_DIR = PROJECT_DIR / "logs"
 CHECKPOINTS_DIR = LOGS_DIR / "model_checkpoints"
 
-DEFAULT_CVAL_SPLITS_PATH = ROOT_DIR / "cross_val_splits.pkl"
+DEFAULT_CVAL_SPLITS_PATH = PROJECT_DIR / "cross_val_splits.pkl"
 
-ASSETS_DIR = ROOT_DIR / "assets"
+ASSETS_DIR = PROJECT_DIR / "assets"
+
+DATA_DIR = Path.home().resolve() / "data"
+PREPROC_DATA_DIR = DATA_DIR / "preproc"
+CENTERLINES_DIR = DATA_DIR / "auto_centerlines"
+CORRECTIONS_DIR = DATA_DIR / "corrections"
+ANNOTATIONS_DIR = DATA_DIR / "annotations"
